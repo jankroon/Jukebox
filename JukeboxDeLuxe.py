2d9e2e63aa717fc8f99e65f64875f0b372644fd6
Jan Kroon <jan@dmo.com>
2015-04-21 21:21:16 +0200

    Initial commit

---
+++ JukeboxDeLuxe.py
@@ -0,0 +1,55 @@
+# Class
+class Jukebox(object):
+    "Python Class that can play popular tunes"
+
+    def __init__(self, name): # This is called a constructor method
+        self.name = name
+        self.coins = 0
+        self.credits = 0
+        self.tunes = []
+
+    def insertCoins(self, n):
+        self.coins = self.coins + n
+        self.credits = self.credits + n
+
+    def playSinatraTune(self):
+        if self.credits > 0:
+            self.credits = self.credits - 1
+            print("doobie doobie doo")
+        else:
+            print("First insert coins, please!")
+
+    def playHappyBirthDay(self, birthDayGirl):
+        if self.credits > 0:
+            self.credits = self.credits - 1
+            print("Happy Birthday to you")
+            print("Happy Birthday to you")
+            print("Happy Birthday dear " + birthDayGirl)
+            print("Happy Birthday to you!")
+        else:
+            print("First insert coins, please!")
+
+# Extended Class, Subclass, Inheritance
+class JukeboxDeLuxe(Jukebox):
+    "Extension of Python Class Jukeboxthat can play more popular tunes"
+
+    def __init__(self, name): # This is called a constructor method
+        Jukebox.__init__(self, name)
+        
+    def playOODenHaag(self):
+        if self.credits > 0:
+            self.credits = self.credits - 1
+            print("O, O Den Haag")
+            print("Mooie stad achter de duinen")
+        else:
+            print("First insert coins, please!")
+
+    def playSinatraTune(self):
+        if self.credits > 0:
+            self.credits = self.credits - 1
+            print("New York, New York")
+            print("I like to wake up in a city that never sleeps!")
+        else:
+            print("First insert coins, please!")
+
+jbdl = JukeboxDeLuxe("Wurlitzer De Luxe")
